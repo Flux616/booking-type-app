9161e3db653863b80f6945ffdc78b43fd74eda0f
Flux616 <flux491067@gmail.com>
2021-09-15 13:48:12 -0700

    build: ios & android set up

--- android/app/src/main/java/com/firsttest/MainActivity.java
+++ android/app/src/main/java/com/firsttest/MainActivity.java
@@ -1,6 +1,7 @@
-package com.shvets;
+package com.firsttest;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -10,6 +11,11 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "shvets";
+    return "firstTest";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
