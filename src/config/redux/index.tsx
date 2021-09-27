4610c0306881ac040a2b7afa90949811d928f710
Flux616 <flux491067@gmail.com>
2021-09-27 07:22:22 -0700

    refactor: migrate redux to TS

--- src/config/redux/index.tsx
+++ src/config/redux/index.tsx
@@ -1,7 +1,6 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import profileSlice from '../../modules/Profile/redux';
+import profileSlice from '../../modules/profile/redux';
 import locationsSlice from '../../modules/location/redux';
-
 
 const rootReducer = combineReducers({
     profile: profileSlice,
@@ -11,3 +10,6 @@ const rootReducer = combineReducers({
 export const store = configureStore({
     reducer: rootReducer
 });
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
