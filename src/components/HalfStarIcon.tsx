203b536cc25c66badbe8cec2885708f0240ae6ff
Flux616 <flux491067@gmail.com>
2021-09-28 01:31:41 -0700

    refactor: migrate svg components to TS

--- src/components/HalfStarIcon.tsx
+++ src/components/HalfStarIcon.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { StyleProp, ViewStyle } from 'react-native';
 import Svg, { Path } from 'react-native-svg';
 
+type Props = {
+    style?: StyleProp<ViewStyle>,
+    height: number,
+    width: number
+}
 
-function HalfStarIcon({style, height, width}) {
+const HalfStarIcon: React.FC<Props> = ({style, height, width}) => {
     return (
         <Svg
             style={style}
             height={height}
             width={width}
             viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
         >
             <Path
                 d="M23.79 9.87l-5.36 5.49 1.26 7.77c.111.62-.562 1.074-1.1.78L12 20.26V.03c.28 0 .56.13.68.4l3.31 7.05 7.37 1.12c.616.11.843.835.43 1.27z"
--- src/components/StarIcon.tsx
+++ src/components/StarIcon.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
+import { ViewStyle, StyleProp } from 'react-native';
 import Svg, { Path } from 'react-native-svg';
 
+type Props = {
+    style?: StyleProp<ViewStyle>,
+    height: number,
+    width: number
+}
 
-function EmptyStarIcon({style, height, width}) {
+const StarIcon: React.FC<Props> = ({style, height, width}) => {
     return (
         <Svg
             style={style}
             height={height}
             width={width}
             viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
         >
             <Path
                 d="M23.363 8.584l-7.378-1.127L12.678.413c-.247-.526-1.11-.526-1.357 0L8.015 7.457.637 8.584a.75.75 0 00-.423 1.265l5.36 5.494-1.267 7.767a.75.75 0 001.103.777L12 20.245l6.59 3.643a.75.75 0 001.103-.777l-1.267-7.767 5.36-5.494a.75.75 0 00-.423-1.266z"
-                fill="#ECEFF1"
+                fill="#ffc107"
             />
         </Svg>
     );
 }
 
-export default EmptyStarIcon;
+export default StarIcon;
