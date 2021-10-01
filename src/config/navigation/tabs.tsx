8e25554ccf667e1dd70066ef82d20b3be4fe0218
Flux616 <flux491067@gmail.com>
2021-10-01 02:27:29 -0700

    feat: localize navigation

--- src/config/navigation/tabs.tsx
+++ src/config/navigation/tabs.tsx
@@ -3,6 +3,7 @@ import Home from '../../modules/home';
 import Profile from '../../modules/profile';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useTranslation } from 'react-i18next';
 
 type Props = {
     iconName: string
@@ -17,24 +18,29 @@ const TabBarIcon: React.FC<Props> = ({iconName, focused, size, color}) => (
     <Icon name={focused ? `${iconName}` : `${iconName}-outline`} size={size} color={focused ? '#4494DA' : color}/>
 )
 
-const HomeTabs = () => (
-    <HomeTab.Navigator screenOptions={{tabBarShowLabel: false}}>
-        <HomeTab.Screen
-            name='Home'
-            component={Home}
-            options={{
-                headerShown: false,
-                tabBarIcon: ({ focused, size, color }) => <TabBarIcon focused={focused} size={size} color={color} iconName='home'/>
-            }}
-        />
-        <HomeTab.Screen
-            name='Profile'
-            component={Profile}
-            options={{
-                tabBarIcon: ({ focused, size, color }) => <TabBarIcon focused={focused} size={size} color={color} iconName='person'/>
-            }}
-        />
-    </HomeTab.Navigator>
-);
+const HomeTabs = () => {
+    const { t } = useTranslation('translation', {keyPrefix: 'screens.profile.header'})
+
+    return (
+        <HomeTab.Navigator screenOptions={{tabBarShowLabel: false}}>
+            <HomeTab.Screen
+                name='Home'
+                component={Home}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({ focused, size, color }) => <TabBarIcon focused={focused} size={size} color={color} iconName='home'/>
+                }}
+            />
+            <HomeTab.Screen
+                name='Profile'
+                component={Profile}
+                options={{
+                    title: t('header'),
+                    tabBarIcon: ({ focused, size, color }) => <TabBarIcon focused={focused} size={size} color={color} iconName='person'/>
+                }}
+            />
+        </HomeTab.Navigator>
+    );
+}
 
 export default HomeTabs;
