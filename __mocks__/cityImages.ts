23b0d16ce1cfeeebf0132f699715df098f25d516
Flux616 <flux491067@gmail.com>
2021-10-19 04:42:28 -0700

    refactor: remove local mocks

--- __mocks__/cityImages.ts
+++ __mocks__/cityImages.ts
@@ -1,18 +1,18 @@
 export const cityImages = [
     {
-        path: require('../assets/images/countries/denmark/copenhagen/2.jpg'),
+        path: 'https://media.cntraveler.com/photos/581a03385a7350de3711d251/master/w_2048,h_1536,c_limit/nuremberg-germany-GettyImages-596693579.jpg',
         id: '1'
     },
     {
-        path: require('../assets/images/countries/denmark/copenhagen/1.jpg'),
+        path: 'https://media.cntraveler.com/photos/5cf7f15cd9fb41110ed08421/master/w_4000,h_2250,c_limit/Prague,-Czech-Republic_GettyImages-929231168.jpg',
         id: '2'
     },
     {
-        path: require('../assets/images/countries/denmark/copenhagen/3.jpg'),
+        path: 'https://mk0travelawayrru2xew.kinstacdn.com/wp-content/uploads/2013/12/prague-old-town-panorama-christmas.jpg',
         id: '3'
     },
     {
-        path: require('../assets/images/countries/denmark/copenhagen/4.jpg'),
+        path: 'https://i.insider.com/5953a2645d3e4e41fd2623e3?width=750&format=jpeg&auto=webp',
         id: '4'
     }
-]
+];
--- __mocks__/profileStore.ts
+++ __mocks__/profileStore.ts
@@ -1,8 +1,8 @@
 export const profileStore = {
-    image: require('../assets/images/avatar.jpg'),
+    image: 'https://pbs.twimg.com/media/EBd2XPrW4AA32iH.jpg',
     name: 'Jimmy Raynor',
     description: 'Kitty Cat',
     email: 'qwerty1234@gmail.com',
     username: 'HappyCat',
     status: 'work'
-}
+};
