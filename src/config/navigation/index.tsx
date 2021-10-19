c6f82513602ac055bcb7d6ef4fcf406b33f69b5b
Flux616 <flux491067@gmail.com>
2021-10-19 04:37:46 -0700

    refactor: connect client to apollo server

--- src/config/navigation/index.tsx
+++ src/config/navigation/index.tsx
@@ -9,34 +9,38 @@ import { RootStackParamList } from './types';
 import { ThemeProvider } from 'styled-components/native';
 import { observer } from 'mobx-react';
 import ThemeStore from '../stores/theme';
+import { ApolloProvider } from '@apollo/client';
+import { client } from '../graphql';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const AppNavigation = () => (
-    <ThemeProvider theme={ThemeStore.currentTheme}>
-        <NavigationContainer>
-            <Stack.Navigator
-                screenOptions={{headerShown: false}}>
-                <Stack.Screen
-                    name='HomeTabs'
-                    component={HomeTabs}
-                />
-                <Stack.Screen
-                    name='Details'
-                    component={Details}
-                />
-                <Stack.Screen
-                    name='Location'
-                    component={Location}
-                />
-                <Stack.Screen
-                    name='EditProfile'
-                    component={EditProfile}
-                    options={getEditScreenNavigationOptions}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+        <ThemeProvider theme={ThemeStore.currentTheme}>
+            <NavigationContainer>
+                <Stack.Navigator
+                    screenOptions={{headerShown: false}}>
+                    <Stack.Screen
+                        name='HomeTabs'
+                        component={HomeTabs}
+                    />
+                    <Stack.Screen
+                        name='Details'
+                        component={Details}
+                    />
+                    <Stack.Screen
+                        name='Location'
+                        component={Location}
+                    />
+                    <Stack.Screen
+                        name='EditProfile'
+                        component={EditProfile}
+                        options={getEditScreenNavigationOptions}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </ThemeProvider>
+    </ApolloProvider>
 );
 
 export default observer(AppNavigation);
